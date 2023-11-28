e74f8fa980dc8b5cf9e1a0168c251e73323a0122
Ethan Groene <ethangroene@gmail.com>
2023-11-27 22:59:06 -0600

    Display dog card for every dog to be shown

--- src/Components/Dogs.tsx
+++ src/Components/Dogs.tsx
@@ -1,11 +1,36 @@
 // Right now these dogs are constant, but in reality we should be getting these from our server
 // Todo: Refactor to get rid of props (THERE SHOULD BE NO PROPS DRILLING ON THIS COMPONENT)
+import { useContext } from "react";
+import { SectionContext } from "../App";
+import { DogCard } from "./DogCard";
+import { Dog } from "../types";
+
 export const Dogs = () =>
   // no props allowed
   {
+    const sectionContextValues = useContext(SectionContext);
+
+    let displayedDogs = sectionContextValues.allDogs;
+    if (sectionContextValues.activeTab === "fav-dogs") {
+      displayedDogs = allDogs.filter((dog) => dog.isFavorite);
+    }
+    if (sectionContextValues.activeTab === "unfav-dogs") {
+      displayedDogs = allDogs.filter((dog) => !dog.isFavorite);
+    }
+
     return (
-      //  the "<> </>"" are called react fragments, it's like adding all the html inside
-      // without adding an actual html element
-      <>{/* Make all the dog cards show up here */}</>
+      <>
+        {displayedDogs.map((dog: Dog) => (
+          <DogCard
+            dog={{
+              id: dog.id,
+              image: dog.image,
+              description: dog.description,
+              isFavorite: dog.isFavorite,
+              name: dog.name,
+            }}
+          />
+        ))}
+      </>
     );
   };
