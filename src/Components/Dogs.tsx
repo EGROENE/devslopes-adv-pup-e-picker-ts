17fae8cfe0319f7225da71024b1e95a1a208e060
Ethan Groene <ethangroene@gmail.com>
2023-11-28 22:46:59 -0600

    Type values from SectionContext

--- src/Components/Dogs.tsx
+++ src/Components/Dogs.tsx
@@ -1,21 +1,27 @@
 // Right now these dogs are constant, but in reality we should be getting these from our server
 // Todo: Refactor to get rid of props (THERE SHOULD BE NO PROPS DRILLING ON THIS COMPONENT)
-import { useContext } from "react";
+import { useContext, Dispatch, SetStateAction } from "react";
 import { SectionContext } from "../App";
 import { DogCard } from "./DogCard";
-import { Dog } from "../types";
+import { Dog, Tab } from "../types";
 
 export const Dogs = () =>
   // no props allowed
   {
-    const sectionContextValues = useContext(SectionContext);
+    const sectionContextValues: {
+      allDogs: Dog[];
+      isLoading: boolean;
+      setIsLoading: Dispatch<SetStateAction<boolean>>;
+      activeTab: Tab;
+      setActiveTab: Dispatch<SetStateAction<Tab>>;
+    } = useContext(SectionContext);
 
     let displayedDogs = sectionContextValues.allDogs;
     if (sectionContextValues.activeTab === "fav-dogs") {
-      displayedDogs = allDogs.filter((dog) => dog.isFavorite);
+      displayedDogs = sectionContextValues.allDogs.filter((dog) => dog.isFavorite);
     }
     if (sectionContextValues.activeTab === "unfav-dogs") {
-      displayedDogs = allDogs.filter((dog) => !dog.isFavorite);
+      displayedDogs = sectionContextValues.allDogs.filter((dog) => !dog.isFavorite);
     }
 
     return (
