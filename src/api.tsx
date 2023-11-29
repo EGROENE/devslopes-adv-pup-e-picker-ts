97cf71c0f3c69d34bde18dd4a184e102bee43934
Ethan Groene <ethangroene@gmail.com>
2023-11-29 14:52:10 -0600

    Add method to add new dog to DB

--- src/api.tsx
+++ src/api.tsx
@@ -1,3 +1,5 @@
+import { Dog } from "./types";
+
 const getAllDogs = (): Promise<unknown> => {
   return fetch("http://localhost:3000/dogs", {
     method: "GET",
@@ -7,8 +9,30 @@ const getAllDogs = (): Promise<unknown> => {
     .catch((error) => console.log("error", error));
 };
 
-const postDog = () => {
-  // fill out method
+const postDog = (newDogCharacteristics: Omit<Dog, "id">): Promise<unknown> => {
+  const myHeaders = new Headers();
+  myHeaders.append("Content-Type", "application/json");
+
+  const raw = JSON.stringify({
+    "name": newDogCharacteristics.name,
+    "image": newDogCharacteristics.image,
+    "description": newDogCharacteristics.description,
+    "isFavorite": false,
+    "id": 0,
+  });
+
+  return fetch("http://localhost:3000/dogs", {
+    method: "POST",
+    headers: myHeaders,
+    body: raw,
+    redirect: "follow",
+  }).then((response) => {
+    if (!response.ok) {
+      throw new Error("Could not create dog. Please try again.");
+    } else {
+      return response.json();
+    }
+  });
 };
 const deleteDogRequest = () => {
   // fill out method
